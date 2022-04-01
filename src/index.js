9e2b3945d9434a2aad4ec49f0f47a08e8a046bbb
SUHAIL AHMAD PARRAY <37875320+suhailparray98@users.noreply.github.com>
2022-04-01 09:50:56 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+ReactDOM.render(
+  <div>
+    <h1>Welcome To React </h1>
+    <p>thi is a React WEB PAGE</p>
+  </div>,
+   document.getElementById("root")
+   );
